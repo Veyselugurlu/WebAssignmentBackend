8962bf9bb7a0be77c9aeefb73e01d64390b4aa7a
Veysel <veyselugurluuuu@gmail.com>
2025-09-11 14:51:23 +0300

    Update CORS settings with frontend URL

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'frontend-w9w9-d88x8dq2q-veyselugurlus-projects.vercel.app',
-      'http://localhost:3000',
-    ],
+    origin: ['https://frontend-w9w9.vercel.app', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
