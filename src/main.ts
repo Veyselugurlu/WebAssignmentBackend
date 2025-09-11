07ffc947e68f2db69844a3b73369ce1d0b5a58d9
Veysel <veyselugurluuuu@gmail.com>
2025-09-11 14:07:17 +0300

    Update CORS settings to allow frontend URLs

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['https://frontend-xi-roan-49.vercel.app', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-
-  // Render veya Heroku gibi servislerin verdiği PORT’u kullan
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
