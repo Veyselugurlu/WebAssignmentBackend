5fd46298b838281ff08513a1290fb0956dffb815
Veysel <veyselugurluuuu@gmail.com>
2025-09-11 13:27:29 +0300

    fix: update main.ts to use process.env.PORT for Render

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(3000);
+  // Render veya Heroku gibi servislerin verdiği PORT’u kullan
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
